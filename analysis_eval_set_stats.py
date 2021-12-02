f76cae4c44129293cbde6c18ba1deec29fc2dc9b
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2021-12-02 14:40:32 +0100

    Update eval set stats for multiple files

--- analysis_eval_set_stats.py
+++ analysis_eval_set_stats.py
@@ -2,33 +2,54 @@ import argparse
 import pickle
 
 import matplotlib.pyplot as plt
+import pandas as pd
 
-
-from utils import convert_eval_sets_to_one_sample_per_row
+from utils import convert_eval_sets_to_one_sample_per_row, SUBJECT, OBJECT
 
 
 def eval_set_stats(args):
-    print("Processing: ", args.input_file)
-    eval_sets = pickle.load(open(args.input_file, "rb"))
+    all_eval_sets = []
+    for input_file in args.input_files:
+        print("Loading: ", input_file)
+        eval_set = pickle.load(open(input_file, "rb"))
+        eval_set = convert_eval_sets_to_one_sample_per_row(eval_set)
 
-    eval_set = convert_eval_sets_to_one_sample_per_row(eval_sets)
+        all_eval_sets.append(eval_set)
 
-    eval_set.groupby("target_word").size().plot.bar()
+    eval_sets = pd.concat(all_eval_sets, ignore_index=True)
+
+    eval_sets.groupby("target_word").size().plot.bar()
     plt.xticks(rotation=75)
     plt.subplots_adjust(bottom=0.3)
     plt.ylabel("# Samples")
+    plt.tight_layout()
+    plt.savefig("results_figures/num_samples_target_word.png")
 
-    plt.figure()
-    eval_set.groupby(["target_word", "distractor_word"]).size().plot.bar()
+    plt.figure(figsize=(20, 10))
+    eval_sets.groupby(["target_word", "distractor_word"]).size().plot.bar()
     plt.xticks(rotation=75)
     plt.subplots_adjust(bottom=0.3)
     plt.ylabel("# Samples")
+    plt.tight_layout()
+    plt.savefig("results_figures/num_samples_target_distractor.png")
+
+    plt.figure(figsize=(20, 10))
+    eval_sets["subject"] = [r[SUBJECT] for r in eval_sets["relationship_target"]]
+    eval_sets["object"] = [r[OBJECT] for r in eval_sets["relationship_target"]]
+    eval_sets["subj_obj"] = [(t, d) for t, d in zip(eval_sets["subject"], eval_sets["object"])]
+    eval_sets.groupby("subj_obj").size().plot.bar()
+    plt.xticks(rotation=75)
+    plt.subplots_adjust(bottom=0.3)
+    plt.ylabel("# Samples")
+    plt.tight_layout()
+    plt.savefig("results_figures/num_samples_subject_object.png")
+
     plt.show()
 
 
 def parse_args():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument(type=str, dest="input_file")
+    argparser.add_argument(type=str, dest="input_files", nargs="+")
 
     args = argparser.parse_args()
 
